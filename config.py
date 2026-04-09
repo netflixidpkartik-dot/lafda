5ef777ea5125ebe6870759c75bc4a10785bd298e
netflixidpkartik-dot <netflixidpkartik@gmail.com>
2026-04-09 17:53:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,51 +1,48 @@
 import os
 
 # Telegram API Credentials
-API_ID = 37827563
-API_HASH = "abca86f59db00e94244dd14df8259ff0"
-BOT_TOKEN = "8611853728:AAFvo4LCgJ2ps5CLkEuqACNCCfG-Ijrsatg"
-LOGGER_BOT_TOKEN = "8358380922:AAH0K338z_9nToZRj54Htg_3UHzpH0nFDOM"
+API_ID = int(os.getenv("37827563", "0"))
+API_HASH = os.getenv("abca86f59db00e94244dd14df8259ff0", "")
+BOT_TOKEN = os.getenv("8611853728:AAFvo4LCgJ2ps5CLkEuqACNCCfG-Ijrsatg", "")
+LOGGER_BOT_TOKEN = os.getenv("8358380922:AAH0K338z_9nToZRj54Htg_3UHzpH0nFDOM", "")
 BOT_USERNAME = "TecxoAdsBot"
 BOT_NAME = "Tecxo Ads 🚀"
 LOGGER_BOT_USERNAME = "TecxoAds2bot"
 
 # Admin Settings
-ADMIN_ID = 8665539217
+ADMIN_ID = int(os.getenv("8665539217", "0"))
 ADMIN_USERNAME = "TecxoChat"
-ADMIN_IDS = [8665539217]
+ADMIN_IDS = [int(os.getenv("8665539217", "0"))]
 
-# Image URLs #must change 
-START_IMAGE = "https://graph.org/file/9878e0f9785f390f5b5a3-2ae056edc4003b40e2.jpg" 
+# Image URLs
+START_IMAGE = "https://graph.org/file/9878e0f9785f390f5b5a3-2ae056edc4003b40e2.jpg"
 BROADCAST_IMAGE = "https://graph.org/file/9878e0f9785f390f5b5a3-2ae056edc4003b40e2.jpg"
 FORCE_JOIN_IMAGE = "https://graph.org/file/9878e0f9785f390f5b5a3-2ae056edc4003b40e2.jpg"
 
 # Force Join Settings
 ENABLE_FORCE_JOIN = False
-MUST_JOIN_CHANNEL_ID = 0  # Updated to actual channel ID
-MUSTJOIN_GROUP_ID = 0    # Updated to actual group ID
-MUST_JOIN_CHANNEL_URL = ""  # Channel invite link or can be use public link t.me/{username} 
-MUSTJOIN_GROUP_URL = ""    # Group invite link
+MUST_JOIN_CHANNEL_ID = 0
+MUSTJOIN_GROUP_ID = 0
+MUST_JOIN_CHANNEL_URL = ""
+MUSTJOIN_GROUP_URL = ""
 
 # Channel and Group IDs
 SETUP_GROUP_ID = 0
 TECH_LOG_CHANNEL_ID = 0
-GROUP_ID = 0 # Private group chat ID for ad skipping
+GROUP_ID = 0
 
 # External Links
 PRIVACY_POLICY_URL = ""
 SUPPORT_GROUP_URL = ""
 UPDATES_CHANNEL_URL = ""
-GUIDE_URL = ""  # guide channel bna k usme video upload krne k baad uska link change krdena 
+GUIDE_URL = ""
 PRIVATE_CHANNEL_INVITE = ""
 
 # Encryption Key
-ENCRYPTION_KEY = "uJ4XPKoW-bK-5J_bB1PjesbA1DDhBvsql-SEV4Qhmzw="
+ENCRYPTION_KEY = os.getenv("uJ4XPKoW-bK-5J_bB1PjesbA1DDhBvsql-SEV4Qhmzw=", "")
 
 # Database Configuration
-MONGO_URI = os.getenv(
-    "mongodb+srv://kartikx17:<wwweeerrr>@cluster0.pm7gb82.mongodb.net/?appName=Cluster0",
-    ""  # use own dont use this one is confedintial
-)
+MONGO_URI = os.getenv("mongodb+srv://kartikx17:<wwweeerrr>@cluster0.pm7gb82.mongodb.net/?appName=Cluster0", "")
 DB_NAME = "adsbot_db"
 
 # Broadcast Settings
@@ -73,7 +70,7 @@ SUCCESS_MESSAGES = {
     "otp_sent": "OTP sent to your phone number!",
     "broadcast_started": "Broadcast started successfully!",
     "broadcast_completed": "Broadcast completed successfully!",
-    "accounts_deleted": "All accounts deleted successfully!"  # Added for delete all accounts
+    "accounts_deleted": "All accounts deleted successfully!"
 }
 
 # Error Messages
